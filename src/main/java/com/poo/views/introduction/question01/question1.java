b5c7fcb9482901b4793d8420233332edfdc9d13e
kennedyAlvess <kennedy.pereira@alunos.ufersa.edu.br>
2024-04-01 22:25:36 -0300

    creating the first response screen v1

--- src/main/java/com/poo/views/introduction/question01/question1.java
+++ src/main/java/com/poo/views/introduction/question01/question1.java
@@ -1,20 +1,43 @@
 package com.poo.views.introduction.question01;
 
+import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.GridBagLayout;
+import java.awt.TextField;
+
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
+public class question1 extends JPanel {
 
-public class question1 extends JPanel{
-    
     public question1() {
-        setLayout(new GridBagLayout());
+        setLayout(new BorderLayout());
+        setBorder(new EmptyBorder(0, 0, 0, 20));
 
         JLabel menuTitlelabel = new JLabel("Questão 01");
         menuTitlelabel.setHorizontalAlignment(JLabel.CENTER);
-        menuTitlelabel.setFont(new Font("Arial", Font.BOLD, 20));
+        menuTitlelabel.setFont(new Font("Arial", Font.BOLD, 25));
+        add(menuTitlelabel, BorderLayout.NORTH);
 
-        setVisible(true);
+        JLabel side = new JLabel("Informe o valor do lado de um quadrado:");
+        side.setFont(new Font("Arial", Font.BOLD, 15));
+        side.setBorder(new EmptyBorder(0, 0, 10, 0));
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+
+        TextField sideValue = new TextField(15);
+
+        centerPanel.setBorder(new EmptyBorder(40, 10, 0, 0));
+        centerPanel.add(side);
+        centerPanel.add(sideValue);
+
+        add(centerPanel, BorderLayout.CENTER);
+
+        JLabel result = new JLabel("O valor do lado do quadrado é: ");
+        result.setFont(new Font("Arial", Font.BOLD, 15));
+        result.setBorder(new EmptyBorder(0, 10, 50, 0));
+        add(result, BorderLayout.SOUTH);
     }
 }
